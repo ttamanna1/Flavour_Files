9263400526b2f338e056c8368a2d46d97ad71e81
Tamanna <tamannakhanum@hotmail.co.uk>
2025-09-03 11:35:04 +0100

    Add home screen and details screen.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,9 @@ android {
 }
 
 dependencies {
+    val nav_version = "2.7.5"
 
+    implementation("androidx.navigation:navigation-compose:$nav_version")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -49,6 +51,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.navigation.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/flavourfiles/MainActivity.kt
+++ app/src/main/java/com/example/flavourfiles/MainActivity.kt
@@ -1,16 +1,28 @@
 package com.example.flavourfiles
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.flavourfiles.ui.theme.FlavourFilesTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +31,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             FlavourFilesTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                ) {
+                    App()
                 }
             }
         }
@@ -31,17 +42,65 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun App() {
+
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "home") {
+
+        composable(route = "home") {
+            HomeScreen(
+                { navController.navigate("details") }
+            )
+        }
+
+        composable(route = "details") {
+            DetailsScreen(
+                { navController.navigate("home") }
+            )
+        }
+    }
+}
+
+@Composable
+fun HomeScreen(onNextScreen: () -> Unit) {
+    Column {
+        Text(
+            modifier = Modifier.fillMaxWidth(),
+            text = "This is the home screen",
+        )
+        Text(
+            modifier = Modifier.fillMaxWidth(),
+            text = stringResource(R.string.recipe_pasta_title),
+        )
+        Image(
+            painter =painterResource(id = R.drawable.virtual_background),
+            contentDescription = null,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(onClick = onNextScreen) {
+            Text("Go to details screen")
+        }
+    }
+}
+
+@Composable
+fun DetailsScreen(onNextScreen: () -> Unit) {
+    Column {
+        Text(
+            modifier = Modifier.fillMaxWidth(),
+            text = "This is the details screen",
+        )
+        Button(onClick = onNextScreen) {
+            Text("Go to home screen")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun FlavourFilesPreview() {
     FlavourFilesTheme {
-        Greeting("Android")
+        App()
     }
 }
