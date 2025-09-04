4b11a2105d602abfa188e7128e88f18aae5691c1
Tamanna <tamannakhanum@hotmail.co.uk>
2025-09-04 14:24:27 +0100

    Add expandable state to view model

--- app/src/main/java/com/example/flavourfiles/FlavourFilesViewModel.kt
+++ app/src/main/java/com/example/flavourfiles/FlavourFilesViewModel.kt
@@ -1,5 +1,9 @@
 package com.example.flavourfiles
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -7,7 +11,9 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 
 data class UiState(
-    val favoriteRecipes: Set<Int> = setOf()
+    val favoriteRecipes: Set<Int> = setOf(),
+    val instructionsClicked: Boolean = false,
+    val methodClicked: Boolean = false
 )
 
 class FlavourFilesViewModel : ViewModel() {
@@ -22,6 +28,15 @@ class FlavourFilesViewModel : ViewModel() {
                 currentState.favoriteRecipes + recipe
             }
             currentState.copy(favoriteRecipes = newFavorites)
+        }
+    }
+
+    fun toggleInstructions() {
+        _uiState.update { it.copy( instructionsClicked = !it.instructionsClicked)
+        }
+    }
+    fun toggleMethods() {
+        _uiState.update { it.copy( methodClicked = !it.methodClicked)
         }
     }
 }
